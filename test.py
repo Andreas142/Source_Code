c774e41e23c21aa48b92d9e12d2117da5afb672d
Andreas142 <91791689+Andreas142@users.noreply.github.com>
2021-11-21 19:01:37 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+%matplotlib inline
 
 #Sidebar
 st.sidebar.text("")
