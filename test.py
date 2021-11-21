ff45d0d5f3c0e4cfdd2880140184696a8822f1b7
Andreas142 <91791689+Andreas142@users.noreply.github.com>
2021-11-21 18:44:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,9 +7,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+
 
 #Sidebar
 st.sidebar.text("")
